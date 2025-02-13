50e9277cf3004dd9bfefdde6b4eb608d6f701c52
khadiijaa123 <Khadijasahnoun70@gmail.com>
2025-02-13 20:46:17 +0100

    je trouve le probleme d'insertion a la base de donnéé

--- app/services/Demandeservice.php
+++ app/services/Demandeservice.php
@@ -17,16 +17,26 @@ class DemandeService{
         return $this->repository->afficherAnnonce($conducteur_id);
     }
 
-
     public function createDemande($request) {
-
-        try{
-            $demande = new Demande(1,1,1, $request['longueur'],$request['largeur'],$request['hauteur'],$request['poids'],$request['depart'],$request['destination'],  $_SESSION['user_id']);
-            if($this->repository->insertDemande($demande)){
+        try {
+            $demande = new Demande(
+                $_SESSION['user_id'],
+                null, 
+                $request['annonce_id'], 
+                'Soumis', 
+                $request['longueur'], 
+                $request['largeur'], 
+                $request['hauteur'], 
+                $request['poids'], 
+                $request['depart'], 
+                $request['destination'] 
+            );
+            if ($this->repository->insertDemande($demande)) {
                 return ['success' => true];
+            } else {
+                return ['success' => false, 'errors' => ['Something went wrong please try again later!']];
             }
-            return ['success' => false, 'errors' => ['Something went wrong please try again later !']];
-        }catch(InputException $e){
+        } catch (InputException $e) {
             return ['success' => false, 'errors' => [$e->getMessage()]];
         }
     }
