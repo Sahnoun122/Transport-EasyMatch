acd75b410e2221421eda61ce171f964727e0c3cc
lakrounehamza <lakrounehamza10@gmail.com>
2025-02-13 13:17:20 +0100

    add method to adminRepository to suspend users

--- app/repositories/AdminRepository.php
+++ app/repositories/AdminRepository.php
@@ -12,4 +12,16 @@ class AdminRepository {
     public function __construct() {
         $this->db = Database::getInstance()->getConnection();
     }
+    public function  suspendreUser($id){
+        try {
+            $sql = "UPDATE users SET  is_suspend = 0 WHERE id = :id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+
+            $stmt->execute();
+        } catch (PDOException $e) {
+            return "Erreur lors de la confirmation demande : ". $e->getMessage();
+        }
+    }
+    // public function accepter
 }
