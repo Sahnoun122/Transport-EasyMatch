bf3903ea556496830b05c9d1bc38c89941418b9c
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-02-13 12:14:05 +0100

    created register method in auth repository

--- app/repositories/AuthRepository.php
+++ app/repositories/AuthRepository.php
@@ -16,9 +16,9 @@ class AuthRepository{
 
     public function getUserByEmailOrId($value){
         if(is_string($value))
-            $emailCheckQuery = 'SELECT * FROM "user" WHERE email = :value';
+            $emailCheckQuery = 'SELECT * FROM Users WHERE email = :value';
         else
-            $emailCheckQuery = 'SELECT * FROM "user" WHERE id = :value';
+            $emailCheckQuery = 'SELECT * FROM Users WHERE id = :value';
 
         $emailCheckStmt = $this->db->prepare($emailCheckQuery);
         $emailCheckStmt->bindValue(':value', $value, is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR);
@@ -26,10 +26,10 @@ class AuthRepository{
         $user = $emailCheckStmt->fetch();
         return $user;
     }
-    
+
     public function login(User $user){
         try{
-            $query = 'SELECT id, role, password FROM "user" WHERE email = :email';
+            $query = 'SELECT id, role, password FROM Users WHERE email = :email';
             $stmt = $this->db->prepare($query);
             $stmt->bindValue(':email', $user->getEmail(), PDO::PARAM_STR);
             $stmt->execute();
@@ -39,4 +39,26 @@ class AuthRepository{
             return null;
         }
     }
+
+    public function register(User $user){
+        try{
+            $hashedPassword = password_hash($user->getPassword(), PASSWORD_DEFAULT);    
+            $query = 'INSERT INTO Users(fname, lname, email, password, role) VALUES(:fname, :lname, :email, :password, :role)';
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(':fname', htmlspecialchars($user->getFname()), PDO::PARAM_STR);
+            $stmt->bindValue(':lname', htmlspecialchars($user->getLname()), PDO::PARAM_STR);
+            $stmt->bindValue(':email', htmlspecialchars($user->getEmail()), PDO::PARAM_STR);
+            $stmt->bindValue(':password', $hashedPassword, PDO::PARAM_STR);
+            $stmt->bindValue(':role', htmlspecialchars($user->getRole()), PDO::PARAM_STR);
+            if($stmt->execute()){
+                return true;
+            }
+    
+            return false;
+        }catch(PDOException $e){
+            Logger::error_log($e->getMessage());
+            return false;
+        }
+    }
+
 }
